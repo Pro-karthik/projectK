cda310ec0a44d5a2bf219577b79d7c38afacaac5
satya karthikeya chapuri <154614124+Pro-karthik@users.noreply.github.com>
2024-03-09 20:00:48 +0530

    Add files via upload

---
+++ lifeSync/lifesync/function.js
@@ -0,0 +1,8 @@
+let wow = document.getElementById("appointment-form");
+let page = document.getElementById("appointment");
+let gone = document.getElementById("gone");
+
+function lol(a){
+  wow.action = a;
+}
+
